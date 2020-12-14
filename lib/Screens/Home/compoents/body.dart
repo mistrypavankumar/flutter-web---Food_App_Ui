2a2e027c165afe3d1e22ca2783c4c1c24699d5cb
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-12-15 00:03:17 +0530

    Added body section

--- lib/Screens/Home/compoents/body.dart
+++ lib/Screens/Home/compoents/body.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import '../../../data/customColor.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Text(
+            "Burger".toUpperCase(),
+            style: Theme.of(context).textTheme.headline1.copyWith(
+              color: kTextColor,
+            ),
+          ),
+          Text(
+            "Qui deserunt ex aute anim est esse commodo deserunt dolore exercitation.",
+            style: TextStyle(
+              fontSize: 21,
+              color: kTextColor.withOpacity(0.34),
+            ),
+          ),
+          FittedBox(
+            // Now it just takes the required space
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF372930),
+                borderRadius: BorderRadius.circular(34),
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    height: 38,
+                    width: 38,
+                    decoration: BoxDecoration(
+                      color: kPrimaryColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFF372930),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Text(
+                    "Get Started".toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
